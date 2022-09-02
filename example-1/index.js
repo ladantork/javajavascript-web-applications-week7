d8dc02500e2a1d4f0fddcbfdac186d2a163cd825
ladantork <ladantorkzaban0@gmail.com>
2022-09-02 12:45:41 +0100

    1,2

---
+++ example-1/index.js
@@ -0,0 +1,12 @@
+// console.log doesn't print anything on the page
+// it is not meant to be visible to the user, but for you
+// to help in debugging and getting visibility in your JS code.
+//
+// on Mac (using Chrome), use Option+Command+J to open the console and see this message.
+const add = require('./add');
+const multiply = require('./multiply');
+
+console.log('Hello from the developer console!');
+const date = Date.now();
+console.log('02/09/2022');
+console.log((2+3)*4);
